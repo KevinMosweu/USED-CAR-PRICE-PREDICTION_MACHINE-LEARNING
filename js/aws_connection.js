96cf1b50ec9c155f28c7de286e2cca0d96c52efb
gusmendesbh <101278278@georgebrown.ca>
2023-06-10 20:33:54 -0400

    updating html and js files

--- js/aws_connection.js
+++ js/aws_connection.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql2');
+const mysql = require('mysql');
 const app = express();
 
 const connection = mysql.createConnection({
@@ -14,38 +14,38 @@ connection.query('SELECT * FROM used_car_prices', (error, results) => {
     console.error('Error executing query:', error);
     return;
   }
-  console.log('Query results:', results);
+  // console.log('Query results:', results);
   // Process the results and pass them to your dashboard visualization code
 });
 
 
 
 
-// // Handle client-side request for data
-// app.get('/api/data', (req, res) => {
-//   // Connect to the database
-//   connection.connect((err) => {
-//     if (err) {
-//       console.error('Error connecting to the database:', err);
-//       res.status(500).send('Error connecting to the database');
-//       return;
-//     }
+// Handle client-side request for data
+app.get('/api/data', (req, res) => {
+  // Connect to the database
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to the database:', err);
+      res.status(500).send('Error connecting to the database');
+      return;
+    }
 
-//     // Execute your SQL query
-//     const query = 'SELECT price, year, manufacturer, model, condition, fuel, odometer, transmission, drive, state FROM your_table';
-//     connection.query(query, (error, results) => {
-//       if (error) {
-//         console.error('Error executing query:', error);
-//         res.status(500).send('Error executing query');
-//         return;
-//       }
-//       console.log('Query results:', results);
-//       // Send the results back as a response
-//       res.json(results);
-//     });
-//   });
-// });
+    // Execute your SQL query
+    const query = 'SELECT price, year, manufacturer, model, condition, fuel, odometer, transmission, drive, state FROM your_table';
+    connection.query(query, (error, results) => {
+      if (error) {
+        console.error('Error executing query:', error);
+        res.status(500).send('Error executing query');
+        return;
+      }
+      // console.log('Query results:', results);
+      // Send the results back as a response
+      res.json(results);
+    });
+  });
+});
 
-// app.listen(3000, () => {
-//   console.log('Server listening on port 3000');
-// });
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
