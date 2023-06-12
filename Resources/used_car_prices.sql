6062efa9c131679754e44d86a35c91464b9f41cc
gusmendesbh <101278278@georgebrown.ca>
2023-06-12 19:22:13 -0400

    removed unnecessary files

--- Resources/used_car_prices.sql
+++ Resources/used_car_prices.sql
@@ -1,9 +1,10 @@
 USE used_car_prices;
 
+DROP TABLE used_car_prices;
+
 CREATE TABLE used_car_prices (
   region VARCHAR(255),
   price INT,
-  year INT,
   manufacturer VARCHAR(255),
   model VARCHAR(255),
   `condition` VARCHAR(255),
@@ -15,10 +16,11 @@ CREATE TABLE used_car_prices (
   drive VARCHAR(255),
   type VARCHAR(255),
   paint_color VARCHAR(255),
-  state VARCHAR(255)
+  state VARCHAR(255),
+  car_age INT
 );
 
-LOAD DATA INFILE "C:\Temp\df_clean.csv"
+LOAD DATA INFILE "C:\Users\gusme\iCloudDrive\Documents\Data Analystics\UOfT - Data Analytics\Projects\project4-group2\Resources\final_car_sales.csv"
 INTO TABLE used_car_prices
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
@@ -26,5 +28,8 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 select count(model) FROM used_car_prices;
+
+DELETE FROM used_car_prices
+WHERE model IN ('others', '1500', '2500', '3500');
 
 SELECT * FROM used_car_prices;
